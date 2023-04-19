c538fdab486f70294ecc343fd0d766a537006445
Ming <68428618+Mingg817@users.noreply.github.com>
2023-04-19 22:22:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-if [ -z "$PORT" ]; then
+if [ -z "$PORT"
+]; then
   echo "环境变量 PORT 没有被设置。"
   exit 1
 fi
 
-if [ -z "$UUID" ]; then
+if [ -z "$UUID"
+]; then
   echo "环境变量 UUID 没有被设置。"
   exit 1
 fi
@@ -61,26 +63,27 @@ cat>/usr/local/etc/xray/config.json<<EOF
             }
         }
     "routing": {
-        "rules": [
-            {
-                "type": "field",
-                "outboundTag": "proxy",
-                "domain": [
-                    "domain:openai.com"
-                ]
-            }
-    },
-    "log": {
-        "access": "/var/log/xray/access.log",
-        "error": "/var/log/xray/error.log",
-        "loglevel": "warning",
-        "dnsLog": true
+            "rules": [
+                {
+                    "type": "field",
+                    "outboundTag": "proxy",
+                    "domain": [
+                        "domain:openai.com"
+                    ]
+                }
+            ]
+        },
+        "log": {
+            "access": "/var/log/xray/access.log",
+            "error": "/var/log/xray/error.log",
+            "loglevel": "warning",
+            "dnsLog": true
+        }
     }
-}
 EOF
 
 
-curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+curl https: //pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 sudo apt -y update
 sudo apt -y install cloudflare-warp
@@ -93,7 +96,7 @@ warp-cli connect
 
 warp-cli enable-always-on
 
-export ALL_PROXY=socks5://127.0.0.1:40000
+export ALL_PROXY=socks5: //127.0.0.1:40000
 
 systemctl stop xray.service
 
